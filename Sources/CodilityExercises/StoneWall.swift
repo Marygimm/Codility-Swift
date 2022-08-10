964bc1936d478772985ffb420efe917bcb0038bf
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-10 12:14:08 +0000

    StoneWall exercise and test
    Not fully understandable

--- Sources/CodilityExercises/StoneWall.swift
+++ Sources/CodilityExercises/StoneWall.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
+//  StoneWall.swift
 //  
 //
 //  Created by Mary Moreira on 10/08/2022.
 //
 
 import Foundation
+
+struct StoneWall {
+    static func solution(_ H : inout [Int]) -> Int {
+        var arrayOfDifElements = [Int]()
+        var countNumberOfBlocks = 0
+        H.forEach { element in
+            while !arrayOfDifElements.isEmpty, let lastElement = arrayOfDifElements.last, element > lastElement {
+                arrayOfDifElements.popLast()
+            }
+            
+            if !arrayOfDifElements.contains(element) {
+                countNumberOfBlocks += 1
+                arrayOfDifElements.append(element)
+            }
+        }
+        return countNumberOfBlocks
+    }
+}
