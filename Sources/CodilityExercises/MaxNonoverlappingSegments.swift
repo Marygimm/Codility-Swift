605f596ad3649c27da008cae84f964190b10cb5f
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-11 17:08:48 +0000

    maxNonoverlappingSegments exercise and test

--- Sources/CodilityExercises/MaxNonoverlappingSegments.swift
+++ Sources/CodilityExercises/MaxNonoverlappingSegments.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
+//  MaxNonoverlappingSegments.swift
 //  
 //
 //  Created by Mary Moreira on 11/08/2022.
 //
 
 import Foundation
+
+struct MaxNonoverlappingSegments {
+    static func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
+        var currentPosition = -1
+        var segmentCount = 0
+        
+        for element in (0..<A.count) {
+            if A[element] > currentPosition {
+                currentPosition = B[element]
+                segmentCount += 1
+            }
+        }
+        
+        return segmentCount
+            
+    }
+}
--- Tests/CodilityExercisesTests/MaxNonoverlappingSegmentsTests.swift
+++ Tests/CodilityExercisesTests/MaxNonoverlappingSegmentsTests.swift
@@ -1,8 +1,18 @@
 //
-//  File.swift
+//  MaxNonoverlappingSegmentsTests.swift
 //  
 //
 //  Created by Mary Moreira on 11/08/2022.
 //
 
-import Foundation
+import XCTest
+@testable import CodilityExercises
+
+final class MaxNonoverlappingSegmentsTests: XCTestCase {
+    func test() {
+        var arrayToTestA = [1,3,7,9,9]
+        var arrayToTestB = [5,6,8,9,10]
+        
+        XCTAssertEqual(MaxNonoverlappingSegments.solution(&arrayToTestA, &arrayToTestB), 3)
+    }
+}
