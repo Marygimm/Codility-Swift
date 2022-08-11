2709fc88fd56dcc7989669365c40196b16551a83
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-11 17:07:52 +0000

    TieRopes Exercises and tests

--- Sources/CodilityExercises/TieRopes.swift
+++ Sources/CodilityExercises/TieRopes.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
+//  TieRopes.swift
 //  
 //
 //  Created by Mary Moreira on 11/08/2022.
 //
 
 import Foundation
+
+struct TieRopes {
+    
+    static func solution(_ K : Int, _ A : inout [Int]) -> Int {
+        
+        var currentLengthCounter = 0
+        var roupes = 0
+        for element in (0..<A.count) {
+            currentLengthCounter += A[element]
+            if currentLengthCounter >= K {
+                roupes += 1
+                currentLengthCounter = 0
+            }
+        }
+        return roupes
+    }
+}
--- Tests/CodilityExercisesTests/TieRopesTests.swift
+++ Tests/CodilityExercisesTests/TieRopesTests.swift
@@ -1,8 +1,18 @@
 //
-//  File.swift
+//  TieRopesTests.swift
 //  
 //
 //  Created by Mary Moreira on 11/08/2022.
 //
 
-import Foundation
+import XCTest
+@testable import CodilityExercises
+
+final class TieRopesTests: XCTestCase {
+    func test(){
+        
+        var testArray1 = [1,2,3,4,1,1,3]
+        
+        XCTAssertEqual(TieRopes.solution(4, &testArray1), 3)
+    }
+}
