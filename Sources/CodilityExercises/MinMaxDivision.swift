d170247651f0c3f892be933adb6431e64ae23b4d
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-13 15:35:03 +0000

    MinMaxDivision exercise and tests

--- Sources/CodilityExercises/MinMaxDivision.swift
+++ Sources/CodilityExercises/MinMaxDivision.swift
@@ -1,8 +1,52 @@
 //
-//  File.swift
+//  MinMaxDivision.swift
 //  
 //
 //  Created by Mary Moreira on 13/08/2022.
 //
 
 import Foundation
+
+struct MinMaxDivision {
+    static func solution(_ K : Int, _ M : Int, _ A : inout [Int]) -> Int {
+        var minValue = 0
+        var maxValue = 0
+        
+        
+        for element in A {
+            maxValue += element
+            minValue = max(element, minValue)
+        }
+        var bestAnswer = maxValue
+
+        while (minValue <= maxValue) {
+            let midPoint = (minValue + maxValue) / 2
+            let blocks = checkBlocks(array: A, guess: midPoint)
+            if (blocks > K) {
+                minValue = midPoint + 1
+            } else {
+                maxValue = midPoint - 1
+                if midPoint < bestAnswer {
+                    bestAnswer = midPoint
+                }
+            }
+        }
+        
+        
+        return bestAnswer
+    }
+    
+    static func checkBlocks(array: [Int], guess: Int) -> Int {
+        var blocks = 1
+        var blockSum = 0
+        
+        for element in array {
+            blockSum += element
+            if blockSum > guess {
+                blockSum = element
+                blocks += 1
+            }
+        }
+        return blocks
+    }
+}
--- Tests/CodilityExercisesTests/MinMaxDivisionTests.swift
+++ Tests/CodilityExercisesTests/MinMaxDivisionTests.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  MinMaxDivisionTests.swift
 //  
 //
 //  Created by Mary Moreira on 13/08/2022.
 //
 
-import Foundation
+import XCTest
+@testable import CodilityExercises
+
+final class MinMaxDivisionTests: XCTestCase {
+    func test() {
+        var arrayTest1 = [2,1,5,1,2,2,2]
+        
+        XCTAssertEqual(MinMaxDivision.solution(3, 5, &arrayTest1), 6)
+    }
+}
