e940a6ec0a3d07e806bb5726e5a7963dd583fbba
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-15 15:50:35 +0000

    triangle fixed exercise

--- Sources/CodilityExercises/Triangle.swift
+++ Sources/CodilityExercises/Triangle.swift
@@ -10,36 +10,16 @@ import Algorithms
 
 struct Triangle {
     static func solution(_ A : inout [Int]) -> Int {
-        var hasTriplet = false
+        guard A.count >= 3 else { return 0 }
+        A = A.sorted { $0 < $1 }
         
-        let possibleCombinations = A.combinations(ofCount: 3)
-        possibleCombinations.forEach { element in
-            if checkIfTripletIsTriange(triplet: element) {
-                hasTriplet = checkIfTripletIsTriange(triplet: element)
+        //we know if we sorted the elements that for sure we will have A[i+2] + A[i] > A[i+1] and A[i+2] + A[i+1] > A[i] so is just one condition missing to test
+        for i in (0..<A.count-2) {
+            //A[i] > A[i-2] - A[i+1]
+            if A[i] + A[i+1] > A[i+2] {
+                return 1
             }
         }
-        
-        return hasTriplet ? 1 : 0
-    }
-    
-    static func checkIfTripletIsTriange(triplet: [Int]) -> Bool {
-        guard triplet.count == 3 else { return false }
-        let valueP = triplet[0]
-        let valueQ = triplet[1]
-        let valueR = triplet[2]
-        
-        return (valueP + valueQ > valueR) && (valueQ + valueR > valueP) && (valueP + valueR > valueQ)
+        return 0
     }
 }
-
-
-extension Array {
-    var combinationsWithoutRepetition: [[Element]] {
-        guard !isEmpty else { return [[]] }
-        return Array(self[1...]).combinationsWithoutRepetition.flatMap {
-            [$0, [self[0]] + $0]
-            
-        }
-    }
-}
-    
