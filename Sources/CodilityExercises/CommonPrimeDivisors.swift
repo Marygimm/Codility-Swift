979feaa1abd08581335ad796d5a8e89a2331c6b0
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-16 22:18:50 +0000

    update common prime divisors

--- Sources/CodilityExercises/CommonPrimeDivisors.swift
+++ Sources/CodilityExercises/CommonPrimeDivisors.swift
@@ -1,7 +1,6 @@
 //
 //  CommonPrimeDivisors.swift
 //  
-//https://github.com/alexgolikov/codility-swift/blob/master/Lessons/12.%20Euclidean%20algorithm/CommonPrimeDivisors.swift
 //  Created by Mary Moreira on 13/08/2022.
 //
 
@@ -15,47 +14,28 @@ struct CommonPrimeDivisors {
             return gcd(b, a % b)
         }
     }
-
-   static func samePrimeDivisors(_ a: Int, _ b: Int) -> Bool {
-        let abGcd = gcd(a, b)
-
-        var aVar = a
-        var bVar = b
-
-        while aVar != 1 {
-            let aGcd = gcd(aVar, abGcd)
-
-            if aGcd == 1 {
-                break
-            }
-
-            aVar /= aGcd
-        }
-
-        if aVar != 1 {
-            return false
-        }
-
-        while bVar != 1 {
-            let bGcd = gcd(bVar, abGcd)
-
-            if bGcd == 1 {
-                break
-            }
-
-            bVar /= bGcd
-        }
-
-        return bVar == 1
-    }
-
+    
     static func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
-        var result = 0
-
-        for i in 0..<A.count {
-            result += samePrimeDivisors(A[i], B[i]) ? 1 : 0
+        let size = A.count
+        var a = 0, b = 0, D = 0, count = 0
+        
+        for i in (0..<size) {
+            a = A[i]
+            b = B[i]
+            D = gcd(a, b)
+            
+            while gcd(a, D) != 1 {
+                a /= gcd(a, D)
+            }
+            while gcd(b, D) != 1 {
+                b /= gcd(b, D)
+            }
+            if a == 1 && b == 1 {
+                count += 1
+            }
         }
-
-        return result
+        return count
     }
+    
+    
 }
