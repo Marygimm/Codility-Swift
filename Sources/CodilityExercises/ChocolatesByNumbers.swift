2b926202bb28ea94ae7d00c61b70af0d2fefd811
Marygimm <marygimm.moreira@bringglobal.com>
2022-08-16 21:48:18 +0000

    chocolates add comments

--- Sources/CodilityExercises/ChocolatesByNumbers.swift
+++ Sources/CodilityExercises/ChocolatesByNumbers.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct ChocolatesByNumbers {
-    
+    //the least common multiple/number of chocolates will give us the number of iteractions
+    // the number of eaten chocolate is calculated by the least common multiple / for step lenght
+    // least common multiple  = a*b/greatest common divisior (is the last divisor getted when remainder is 0) 21 and 9 = GCD (3)
     static func solution(_ N : Int, _ M : Int) -> Int {
         return N/greatesCommonDivisor(elementOne: N, elementTwo: M)
      
